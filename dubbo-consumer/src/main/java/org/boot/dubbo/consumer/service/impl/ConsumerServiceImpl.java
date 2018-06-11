93ca801e5cd7c2936c520426038604a85ca44dad
maji <ma.jay@foxmail.com>
2018-06-11 13:17:56 +0800

    init project

---
+++ dubbo-consumer/src/main/java/org/boot/dubbo/consumer/service/impl/ConsumerServiceImpl.java
@@ -0,0 +1,27 @@
+package org.boot.dubbo.consumer.service.impl;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import lombok.extern.slf4j.Slf4j;
+import org.boot.dubbo.consumer.service.ConsumerService;
+import org.boot.dubbo.consumer.service.HelloService;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @author luoliang
+ * @date 2018/1/8
+ **/
+@Service
+@Slf4j
+public class ConsumerServiceImpl implements ConsumerService {
+    @Reference(version = "1.0.0",
+            application = "${dubbo.application.id}")
+    private HelloService helloService;
+
+    @Override
+    public String sayHello(String name) {
+        log.debug("invoke service {}", helloService.getClass());
+        return helloService.sayHello(name);
+    }
+}
